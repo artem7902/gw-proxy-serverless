b8db6a181d1c7fd1a7cfbb2bbabdaad9b578380e
Abhi <abhi.coderx@gmail.com>
2020-02-29 13:10:55 +0530

    Added test cases for post

--- tests/unit/lambdas/gw/proxy/test_saas_vps.py
+++ tests/unit/lambdas/gw/proxy/test_saas_vps.py
@@ -16,48 +16,61 @@ class test_saas_vps(Test_Helper):
     def test_update_lambda(self):
         self.aws_lambda.update_code()
 
-    def test__invoke_directy(self):
-        #payload = {'path':'/favicon-shard.png'}
+    def test__invoke_get_directy(self):
         payload = {'path': '/', 'httpMethod': 'GET', 'headers': {'aaa': 'bbbb'}}
         self.result = self.client.request_get(payload)
 
-    def test__invoke_directy__glasswall(self):
+    def test__invoke_get_directy__glasswall(self):
         payload = {'path': '/', 'httpMethod': 'GET', 'headers': {}, 'requestContext': {'domainPrefix': 'glasswall'} }
         self.result = self.client.request_get(payload)
 
-    def test__invoke_directy__send_firefox_com(self):
+    def test__invoke_get_directy__send_firefox_com(self):
         payload = {'path': '/', 'httpMethod': 'GET', 'headers': {}, 'requestContext': {'domainPrefix': 'send_firefox_com'}}
         self.result = self.client.request_get(payload)
 
-    def test__invoke_via_lambda(self):
-        #payload = {'path': '/favicon-shard.png'}
+    def test__invoke__get_via_lambda(self):
         payload = {'path': '/', 'httpMethod': 'GET', 'headers': {'aaa': 'bbbb'}}
         self.test_update_lambda()
         self.result = self.aws_lambda.invoke(payload)
-        #self.png_data = self.result.get('body')
 
-    def test__invoke_via_lambda__glasswall(self):
-        #payload = {'path': '/wp-content/uploads/2019/12/deepfile-340.png', 'requestContext': {'domainPrefix': 'glasswall'}}
+    def test__invoke__get_via_lambda__glasswall(self):
         payload = {'path': '/', 'httpMethod': 'GET', 'headers': {'aaa': 'bbbb'}, 'requestContext': {'domainPrefix': 'glasswall'} }
         self.test_update_lambda()
         self.result = self.aws_lambda.invoke(payload)
 
-    # def test_invoke_directly_with_payload(self):
-    #     payload = {'resource': '/', 'path': '/', 'httpMethod': 'GET', 'headers': {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-GB,en;q=0.9,pt-PT;q=0.8,pt;q=0.7,en-US;q=0.6', 'Host': 'gw-proxy.com', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'none', 'sec-fetch-user': '?1', 'upgrade-insecure-requests': '1', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36', 'X-Amzn-Trace-Id': 'Root=1-5e48be59-9bbf9c36970c2c503b01ae20', 'X-Forwarded-For': '82.39.36.190', 'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https'}, 'multiValueHeaders': {'accept': ['text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'], 'accept-encoding': ['gzip, deflate, br'], 'accept-language': ['en-GB,en;q=0.9,pt-PT;q=0.8,pt;q=0.7,en-US;q=0.6'], 'Host': ['gw-proxy.com'], 'sec-fetch-mode': ['navigate'], 'sec-fetch-site': ['none'], 'sec-fetch-user': ['?1'], 'upgrade-insecure-requests': ['1'], 'User-Agent': ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'], 'X-Amzn-Trace-Id': ['Root=1-5e48be59-9bbf9c36970c2c503b01ae20'], 'X-Forwarded-For': ['82.39.36.190'], 'X-Forwarded-Port': ['443'], 'X-Forwarded-Proto': ['https']}, 'queryStringParameters': None, 'multiValueQueryStringParameters': None, 'pathParameters': None, 'stageVariables': None, 'requestContext': {'resourceId': 'g72apoktf2', 'resourcePath': '/', 'httpMethod': 'GET', 'extendedRequestId': 'H-KuBFP3joEFiuQ=', 'requestTime': '16/Feb/2020:04:00:25 +0000', 'path': '/', 'accountId': '311800962295', 'protocol': 'HTTP/1.1', 'stage': 'Prod', 'domainPrefix': 'gw-proxy', 'requestTimeEpoch': 1581825625683, 'requestId': '3ff4b3e5-5503-4750-9ef5-cf61b888d552', 'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None, 'caller': None, 'sourceIp': '82.39.36.190', 'principalOrgId': None, 'accessKey': None, 'cognitoAuthenticationType': None, 'cognitoAuthenticationProvider': None, 'userArn': None, 'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36', 'user': None}, 'domainName': 'gw-proxy.com', 'apiId': 'l2zujsuve3'}}
-    #     self.result = run(payload,{})
-    #
-    # def test_invoke_directly__GET_root_path(self):
-    #     payload = { 'path': '/aaa', 'httpMethod': 'GET', 'headers': {'accept': 'text/html,application/xhtml+xml,application/xml'}}
-    #     self.result = run(payload)
+    def test__invoke_post_directy(self):
+        payload = {'path': '/', 'httpMethod': 'POST', 'headers': {'aaa': 'bbbb'}
+                   , 'body': {'user': 'someuser', 'password': 'somepass#543'}}
+        self.result = self.client.request_get(payload)
+
+    def test__invoke_post_directy__glasswall(self):
+        payload = {'path': '/', 'httpMethod': 'POST', 'headers': {},
+                   'requestContext': {'domainPrefix': 'glasswall'},
+                   'body': {'user': 'someuser', 'password': 'somepass#543'} }
+        self.result = self.client.request_get(payload)
+
+    def test__invoke_post_directy__send_firefox_com(self):
+        payload = {'path': '/', 'httpMethod': 'POST', 'headers': {}, 'requestContext':
+            {'domainPrefix': 'send_firefox_com'},
+            'body': {'user': 'someuser', 'password': 'somepass#543'}}
+        self.result = self.client.request_get(payload)
+
+    def test__invoke__post_via_lambda(self):
+        payload = {'path': '/', 'httpMethod': 'GET', 'headers': {'aaa': 'bbbb'}
+                   , 'body': {'user': 'someuser', 'password': 'somepass#543'}}
+        self.test_update_lambda()
+        self.result = self.aws_lambda.invoke(payload)
+
+    def test__invoke__post_via_lambda__glasswall(self):
+        payload = {'path': '/', 'httpMethod': 'GET', 'headers': {'aaa': 'bbbb'},
+                   'requestContext': {'domainPrefix': 'glasswall'}
+                   , 'body': {'user': 'someuser', 'password': 'somepass#543'}}
+        self.test_update_lambda()
+        self.result = self.aws_lambda.invoke(payload)
 
 
-
-
-
-
-
-
-# this call and methods contain the multiple bits required to set up the proxy (todo: consolidate in one end-to-end solution to be executed on a clear AWS region/organisation)
+# this call and methods contain the multiple bits required to set up the proxy (todo: consolidate in one end-to-end
+#  solution to be executed on a clear AWS region/organisation)
 class test_Rest_API__SaaS_VPs(Test_Helper):
 
     def setUp(self):
