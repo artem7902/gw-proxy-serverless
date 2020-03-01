81266bdeefcf511ffebf141f8ccb27adb4772e60
Dinis Cruz <dinis.cruz@owasp.org>
2020-03-01 10:36:46 +0000

    Added tests for Http_Proxy

--- tests/api/test_Http_Proxy.py
+++ tests/api/test_Http_Proxy.py
@@ -11,3 +11,17 @@ class test_Http_Proxy(TestCase):
     def test_ctor(self):
         self.assertIsInstance(self.http_proxy, Http_Proxy)
         self.assertIs(type(self.http_proxy).__name__ , 'Http_Proxy')
+
+    
+    def test_bad_request(self):
+        self.assertEqual(self.http_proxy.bad_request('el-body'),  {'statusCode': 400, 'body': 'el-body'})
+
+    def test_server_error(self):
+        self.assertEqual(self.http_proxy.server_error('el-body'),  {'statusCode': 500, 'body': 'el-body'})
+
+    def test_server_ok(self):
+        is_base_64 = False
+        headers    = {'an': 'header'}
+        body       = 'el-body'
+
+        self.assertEqual(self.http_proxy.ok(headers, body,is_base_64),  {'isBase64Encoded': is_base_64, 'statusCode': 200, 'headers':headers, 'body': 'el-body'})
--- tests/api/test_Lambda_Event.py
+++ tests/api/test_Lambda_Event.py
@@ -10,6 +10,27 @@ class test_Lambda_Event(TestCase):
         self.lambda_event = Lambda_Event(self.params)
 
     # todo: improve test
-    def test_ctor(self): 
+    def test_ctor(self):
         self.assertEquals(self.lambda_event.lambda_data    , {'body': {}, 'path': '', 'method': '', 'domain_prefix': None, 'headers': {}})
         self.assertEquals(self.lambda_event.http_proxy.body, {})
+
+    def test_domain_parser(self):
+        domain_parser = self.lambda_event.domain_parser
+        self.assertEqual(domain_parser('stackoverflow', '/AAAAA'), 'https://stackoverflow.com/AAAAA'                    )
+        self.assertEqual(domain_parser('glasswall'    , '/AA/AA'), 'https://www.glasswallsolutions.com/AA/AA'           )
+        self.assertEqual(domain_parser('gw-proxy'     , '/A/A/A'), 'https://glasswall-file-drop.azurewebsites.net/A/A/A')
+        self.assertEqual(domain_parser('aaaa'         , '/AAAAA'), 'https://aaaa/AAAAA'                                 )
+        self.assertEqual(domain_parser('aaaa'         , '/A?a=b'), 'https://aaaa/A?a=b'                                 )
+        self.assertEqual(domain_parser('aaaa'         , '/A#a=b'), 'https://aaaa/A#a=b'                                 )
+        self.assertEqual(domain_parser('aaaa'         , '/A?a#b'), 'https://aaaa/A?a#b'                                 )
+        self.assertEqual(domain_parser('aaaa'         , 'BBB/cc'), 'https://aaaa/BBB/cc'                                )
+
+        ## handle null values
+        self.assertEqual(domain_parser( None          , '/AAAAA'), 'https://glasswall-file-drop.azurewebsites.net/AAAAA')
+        self.assertEqual(domain_parser('aaaa'         , None    ), 'https://aaaa'                                       )
+
+        ## with security implications
+        self.assertEqual(domain_parser('abc.com/aaa/' , None    ), 'https://abc.com/aaa/'                               ) # domain adds path
+        self.assertEqual(domain_parser('../aaa/'      , None    ), 'https://../aaa/'                                    ) # invalid domain
+        self.assertEqual(domain_parser('a:b@cc'       , None    ), 'https://a:b@cc'                                     ) # domain contain pwd
+        self.assertEqual(domain_parser('aaa'          , '/../aa'), 'https://aaa/../aa'                                  ) # path transversal
