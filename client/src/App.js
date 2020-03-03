260ac5ddfde8e149ad3e7d3852386742f808e966
dev-artisan <leomay3993upwork@gmail.com>
2020-03-03 14:38:58 -0500

    react-wireframe-2

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactSpeedometer from 'react-d3-speedometer';
+import LineChart from './components/lineChart';
+import BarChart from './components/barChart';
+import KeySuccess from './components/keySuccess';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'semantic-ui-css/semantic.min.css';
 
-import ReactSpeedometer from 'react-d3-speedometer';
 import { Row, Col, Container } from 'reactstrap';
-import NewProductTable from './components/newProductTable';
-import NewKeySuccess from './components/newKeySuccess';
-import ProfitabilityTable from './components/profitabilityTable';
-import RoadMap from './components/roadMap';
-import Product from './components/product';
-import KeySuccess from './components/keySuccess';
+import { Card, Icon } from 'semantic-ui-react';
+
 function App() {
   return (
     <div className="App">
       <Container>
         <Row className="mt-5">
-          <div className="mt-1">
-            <div className="title">Engineering</div>
-            <div className="title">Roadmap</div>
-          </div>
+          <div className="title">SALES, REVENUE, SOW + PIPELINE</div>
           <div className="ml-auto" style={{ height: 200 }}>
             <ReactSpeedometer value={500} />
           </div>
         </Row>
-        <Row>
-          <RoadMap />
-        </Row>
-        <div className="products">
-          <Product name="Product 1" />
-          <Product name="Product 2" />
-          <Product name="Product 3" />
-        </div>
-        <div className="subtitle">New Product Development</div>
-        <Row>
-          <Col md={6}>
-            <NewProductTable />
-          </Col>
-          <Col md={6}>
-            <NewKeySuccess />
-          </Col>
-        </Row>
-        <div className="subtitle">Profitability Per Product/Service</div>
+        <Card fluid>
+          <Card.Content header="SALES" />
+          <Card.Content>
+            <LineChart />
+          </Card.Content>
+        </Card>
+
+        <Card fluid>
+          <Card.Content header="REVENUE" />
+          <Card.Content>
+            <LineChart revenue={true} />
+          </Card.Content>
+        </Card>
 
         <Row>
           <Col md={6}>
-            <ProfitabilityTable />
+            <Card fluid>
+              <Card.Content header="PIPELINE + SOW" />
+              <Card.Content>
+                <BarChart />
+              </Card.Content>
+            </Card>
           </Col>
-          <Col md={6}>
+          <Col md={6} className="d-flex justify-content-center">
             <KeySuccess />
           </Col>
         </Row>
