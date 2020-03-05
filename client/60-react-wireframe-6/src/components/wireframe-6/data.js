6d66287bb646b0cacbec9ac28fb4b8e083e0157a
xamqrexii <usama1630@gmail.com>
2020-03-05 23:58:13 +0500

    created data file for wireframe 6

---
+++ client/60-react-wireframe-6/src/components/wireframe-6/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    "key": "1",
+    "keyMatrics": "AVERAGE DEX SIZE",
+    "thisMonth": 32,
+    "lastMonth": 32
+  },
+  {
+    "key": "2",
+    "keyMatrics": "AVERAGE DAYS TO PAYMENT",
+    "thisMonth": 32,
+    "lastMonth": 60
+  },
+  {
+    "key": "3",
+    "keyMatrics": "AVERAGE WIP",
+    "thisMonth": 43,
+    "lastMonth": 32
+  },
+  {
+    "key": "4",
+    "keyMatrics": "AVERAGE DEST.",
+    "thisMonth": 29,
+    "lastMonth": 20
+  }
+]
